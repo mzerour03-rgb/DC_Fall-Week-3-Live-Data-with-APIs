d0eeda4b1d23f1dd610ae7403f9c9f7bef562ded
mzerour03-rgb <mzerour03@gmail.com>
2025-11-01 09:58:54 -0500

    change3

--- scripts/dogApi.js
+++ scripts/dogApi.js
@@ -8,7 +8,7 @@ function apiApp() {
       try {
         // STEP 1: Look at https://dog.ceo/api/breeds/image/random/3
         // STEP 2: Add URL below
-        // const res = await axios.get(Url);
+        // const res = await axios.get(https://dog.ceo/api/breeds/image/random/3);
         // STEP 3:console.log(res.data)
         // STEP 4: 
         this.render(res.data.message);
